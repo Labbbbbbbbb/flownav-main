64fc79625b715a1309859d92c802a22a824b25d1
Labbbbbbbbb <724757025@qq.com>
2026-04-24 18:45:30 +0800

    添加部署命令

---
+++ deploy_cli.sh
@@ -0,0 +1,26 @@
+#ps 不是脚本，只是把命令写出来方便粘贴
+#terminal1
+conda activate nomad_deployment
+cd /home/zyt/flownav-main/deployment/src/navigation
+PYTHONPATH=/home/zyt/flownav-main:/home/zyt/flownav-main/consistency-policy:/home/zyt/flownav-main/py-meanflow:$PYTHONPATH
+
+python navigate_ros1.py --model flownav --dir ../../topomaps/images --ckpt /home/zyt/flownav-main/meanflownav_2026_04_15_20_23_25/latest.pth
+
+#terminal2
+conda activate nomad_deployment
+cd /home/zyt/flownav-main/deployment/src
+PYTHONPATH=/home/zyt/flownav-main:/home/zyt/flownav-main/consistency-policy:/home/zyt/flownav-main/py-meanflow:$PYTHONPATH
+
+python pd_controller.py
+
+#terminal3
+cd ~/catkin_ws
+source devel/setup.bash
+roslaunch realsense2_camera rs_camera.launch
+
+#terminal4
+cd ~/catkin_ws
+source devel/setup.bash
+sudo modprobe gs_usb
+rosrun scout_bringup bringup_can2usb.bash
+roslaunch scout_bringup scout_robot_base.launch 
