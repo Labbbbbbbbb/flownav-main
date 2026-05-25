fabc161bbf43c4f77c124506211d3a49ce2b09fd
Labbbbbbbbb <724757025@qq.com>
2026-05-25 13:36:02 +0800

    修改lora-1分支的可视化代码,添加explore代码

--- deployment/src/exploration/explore_ros1.py
+++ deployment/src/exploration/explore_ros1.py
@@ -1,5 +1,3 @@
-
-
 import os
 import cv2
 
@@ -25,44 +23,17 @@ from std_msgs.msg import Bool, Float32MultiArray,MultiArrayDimension
 # FlowNav / MeanFlow 核心组件
 import torchdiffeq
 from flownav.training.utils import get_action
-from utils import to_numpy, transform_images, load_model, msg_to_pil
+from utils import to_numpy, transform_images, load_model, remove_files_in_dir, msg_to_pil
+
 
 # Flow_Correct /VLM Scorer 组件
 from reward.flow_correct import TrajectoryProjector
 from reward.vlm_trajectory_scorer import VLMTrajectoryScorer
 
-ACTION_STATS = {
-    'min': np.array([-2.5, -4]),
-    'max': np.array([5, 4])
-}
-
-# --- 从你的相机参数中提取 ---
-CAMERA_HEIGHT = 0.4446
-CAMERA_X_OFFSET = 0.3612
-CAMERA_MATRIX = np.array([
-    [386.548, 0.0, 328.326],
-    [0.0, 386.171, 243.573],
-    [0.0, 0.0, 1.0]
-])
-DIST_COEFFS = np.array([-0.054461, 0.063385, 0.000409, -0.000540, -0.019851, 0.0, 0.0, 0.0])
-VIZ_IMAGE_SIZE = (640, 480) # 根据 cx, cy 推断出的分辨率
-
-
-
-
-# CONSTANTS
-TOPOMAP_IMAGES_DIR = "../topomaps/images"
-MODEL_WEIGHTS_PATH = "../model_weights"
-ROBOT_CONFIG_PATH ="../config/robot.yaml"
-MODEL_CONFIG_PATH = "../config/models.yaml"
-with open(ROBOT_CONFIG_PATH, "r") as f:
-    robot_config = yaml.safe_load(f)
-MAX_V = robot_config["max_v"]
-MAX_W = robot_config["max_w"]
-RATE = robot_config["frame_rate"] 
-
-# Visualization colors
-COLORS = ['cyan', 'magenta', 'yellow', 'lime', 'red']
+# Ros Topics
+from topic_names import (IMAGE_TOPIC,
+                        WAYPOINT_TOPIC,
+                        SAMPLED_ACTIONS_TOPIC)
 
 
 # 配置路径（请根据你的实际情况检查这些路径）
@@ -80,7 +51,6 @@ RATE = robot_config["frame_rate"]
 # 话题名称（根据你的 Scout 机器人修改）
 IMAGE_TOPIC = "/camera/color/image_raw" 
 WAYPOINT_TOPIC = "/waypoint"
-REACHED_GOAL_TOPIC = "/topoplan/reached_goal"
 OVERLAY_TOPIC = "/overlay_image"            #pub
 # TRAJS_TOPIC = "/candidate_trajs"
 
@@ -101,7 +71,6 @@ def tensor_to_rgb_uint8(image_tensor: torch.Tensor) -> np.ndarray:
 
 def callback_obs(msg):
     """处理来自 Realsense 的 ROS 1 图像消息"""
-    global obs_img, context_queue
     # 注意：在 Python 3.10 下，我们使用 utils.py 里的 msg_to_pil 绕过 cv_bridge 兼容性问题
     obs_img = msg_to_pil(msg)
     
@@ -112,6 +81,24 @@ def callback_obs(msg):
             context_queue.pop(0)
             context_queue.append(obs_img)
 
+
+ACTION_STATS = {
+    'min': np.array([-2.5, -4]),
+    'max': np.array([5, 4])
+}
+
+# --- 从你的相机参数中提取 ---
+CAMERA_HEIGHT = 0.4446
+CAMERA_X_OFFSET = 0.3612
+CAMERA_MATRIX = np.array([
+    [386.548, 0.0, 328.326],
+    [0.0, 386.171, 243.573],
+    [0.0, 0.0, 1.0]
+])
+DIST_COEFFS = np.array([-0.054461, 0.063385, 0.000409, -0.000540, -0.019851, 0.0, 0.0, 0.0])
+VIZ_IMAGE_SIZE = (640, 480) # 根据 cx, cy 推断出的分辨率
+
+COLORS = ['cyan', 'magenta', 'yellow', 'lime', 'red']
 def init_visualization():
     """Initialize matplotlib figure with two subplots."""
     global fig, ax_left, ax_right
@@ -212,11 +199,11 @@ def visualize_navigation(
     ax_left.legend(loc='upper right', fontsize=8)
     
     # --- 右图处理 ---
-    ax_right.imshow(topomap_img)
-    ax_right.set_title(f'TopoMap Goal (Node {closest_node}/{goal_node})')
+    if topomap_img is not None:
+        ax_right.imshow(topomap_img)
+        ax_right.set_title(f'TopoMap Goal (Node {closest_node}/{goal_node})')
     
     plt.pause(0.01)
-
 
 
 
@@ -232,8 +219,18 @@ def msg_from_numpy(rgb: np.ndarray, stamp=None, frame_id="camera"):
     msg.data = rgb.tobytes()
     return msg
 
+def save_images_and_actions(self):
+    if cur_img is not None and cur_naction is not None:
+        print(f"Saving Image and action {im_idx}")
+        cur_img.save(f"{cur_exp_im_dir}/{im_idx}.png")
+        
+        with open(f"{cur_exp_pkl_dir}/{im_idx}.pkl", "wb") as f:
+            pickle.dump(cur_naction, f)
+            
+        self.im_idx += 1
+
 def main(args):
-    global context_size, obs_img, trajs_msg
+    global context_size,im_idx,cur_img,cur_naction,cur_exp_pkl_dir,cur_exp_im_dir
     
     # 1. 加载模型配置
     with open(MODEL_CONFIG_PATH, "r") as f:
@@ -245,6 +242,18 @@ def main(args):
 
     context_size = model_params["context_size"]
     
+    exp_dir = args.exp_dir
+    os.makedirs(exp_dir, exist_ok=True)
+    cur_img = None
+    cur_naction = None
+    ckpt_path = Path(args.ckpt)
+    cur_exp_dir = f"{exp_dir}/{args.model}_{ckpt_path.name}_{args.dir}_{args.k_steps}"
+    os.makedirs(cur_exp_dir, exist_ok=True)
+    cur_exp_im_dir = f"{cur_exp_dir}/images"
+    os.makedirs(cur_exp_im_dir, exist_ok=True)
+
+    cur_exp_pkl_dir = f"{cur_exp_dir}/pkl"
+    os.makedirs(cur_exp_pkl_dir, exist_ok=True)
     
     # 初始化轨迹消息格式
     trajs_msg = Float32MultiArray()
@@ -268,36 +277,42 @@ def main(args):
     model.eval()
 
     # 3. 加载拓扑地图
-    topomap_dir = os.path.join(TOPOMAP_IMAGES_DIR, args.dir)
-    topomap_filenames = sorted(os.listdir(topomap_dir), key=lambda x: int(x.split(".")[0]))
-    topomap = []
-    print(f"[*] 正在加载拓扑图: {args.dir}, 共 {len(topomap_filenames)} 节点")
-    for fname in topomap_filenames:
-        topomap.append(PILImage.open(os.path.join(topomap_dir, fname)))
+    dir_name = args.dir
+    topomap_name_dir = os.path.join(TOPOMAP_IMAGES_DIR, dir_name)
+    dt = dt
+    img_idx = 0
+
+
+    if not os.path.isdir(topomap_name_dir):
+        os.makedirs(topomap_name_dir)
+    else:
+        print(f"{topomap_name_dir} already exists. Removing previous images...")
+        remove_files_in_dir(topomap_name_dir)
+        
+    print("Waiting for images...")
+
 
     closest_node = 0
-    goal_node = len(topomap) - 1 if args.goal_node == -1 else args.goal_node
+    im_idx = 0
     
     # 4. Scorer初始化
     Trajprojector = TrajectoryProjector(dataset_name="deploy")
     Scorer=VLMTrajectoryScorer()
+
     
-    # 5. 可视化初始化
-    # visualizer = GoalVisualizer(display=True, save=False)
-    
-    # 6. ROS 1 节点初始化
+    # 5. ROS 1 节点初始化
     rospy.init_node("MEANFLOW_NAVIGATION", anonymous=False)
     rospy.Subscriber(IMAGE_TOPIC, Image, callback_obs, queue_size=1)
     waypoint_pub = rospy.Publisher(WAYPOINT_TOPIC, Float32MultiArray, queue_size=1)
-    goal_pub = rospy.Publisher(REACHED_GOAL_TOPIC, Bool, queue_size=1)
+    sampled_actions_pub = rospy.Publisher(SAMPLED_ACTIONS_TOPIC, Float32MultiArray, queue_size=1)
     # trajs_pub = rospy.Publisher(TRAJS_TOPIC, Float32MultiArray, queue_size=1)
     overlay_pub = rospy.Publisher(OVERLAY_TOPIC, Image, queue_size=1)
-    
+    timer_save = rospy.Timer(rospy.Duration(1.0), lambda event: save_images_and_actions())
     ros_rate = rospy.Rate(RATE)
     print(f"[*] ROS 1 节点就绪。等待图像话题: {IMAGE_TOPIC}")
     annotated_image_msg = None
 
-    # 7. 主循环
+    # 8. 主循环
     while not rospy.is_shutdown():
         chosen_waypoint = np.zeros(4)
 
@@ -306,16 +321,8 @@ def main(args):
             obs_images = transform_images(context_queue, model_params["image_size"], center_crop=False)
             obs_images = torch.split(obs_images, 3, dim=1)  #因为拼接是在通道维度上，所以这里按3在dim=1上分割
             obs_images = torch.cat(obs_images, dim=1).to(device)
-            mask = torch.zeros(1).long().to(device)
-
-            # 局部搜索范围（Radius）（要求topomap的图片是有序的）
-            start = max(closest_node - args.radius, 0)
-            end = min(closest_node + args.radius + 1, goal_node)
-            
-            # 预处理目标节点图像 即在当前位置上可以作为下一个目标的图片
-            goal_images = [transform_images(g_img, model_params["image_size"], center_crop=False).to(device) 
-                           for g_img in topomap[start:end + 1]]
-            goal_images = torch.concat(goal_images, dim=0)
+            fake_goal = torch.randn((1, 3, *model_params["image_size"])).to(device)
+            mask = torch.ones(1).long().to(device) # ignore the goal
 
             # 模型推理（带时间戳）
             with torch.no_grad():
@@ -325,34 +332,17 @@ def main(args):
 
                 # vision encoder
                 t_vision_start = time.perf_counter()
-                obs_repeat = obs_images.repeat(len(goal_images), 1, 1, 1)
-                mask_repeat = mask.repeat(len(goal_images))
-                obsgoal_cond = model('vision_encoder', obs_img=obs_repeat, goal_img=goal_images, input_goal_mask=mask_repeat)
+
+                obs_cond = model('vision_encoder', obs_img=obs_images, goal_img=fake_goal, input_goal_mask=mask)
+                
                 t_vision_end = time.perf_counter()
                 timeline["vision_start_ts"] = datetime.now().isoformat()
                 timeline["vision_ms"] = (t_vision_end - t_vision_start) * 1000.0
 
-                # distance prediction
-                t_dist_start = time.perf_counter()
-                dists = model("dist_pred_net", obsgoal_cond=obsgoal_cond)
-                t_dist_end = time.perf_counter()
-                dists = to_numpy(dists.flatten())
-                timeline["dist_start_ts"] = datetime.now().isoformat()
-                timeline["dist_ms"] = (t_dist_end - t_dist_start) * 1000.0
-
-                min_idx = np.argmin(dists)
-                closest_node = min_idx + start
-
-                # choose subgoal and prepare obs_cond
-                t_select_start = time.perf_counter()
-                sg_idx = min(min_idx + int(dists[min_idx] < args.close_threshold), len(obsgoal_cond) - 1)
-                obs_cond = obsgoal_cond[sg_idx].unsqueeze(0)
                 if len(obs_cond.shape) == 2:
                     obs_cond = obs_cond.repeat(args.num_samples, 1)
                 else:
                     obs_cond = obs_cond.repeat(args.num_samples, 1, 1)
-                t_select_end = time.perf_counter()
-                timeline["select_ms"] = (t_select_end - t_select_start) * 1000.0
 
                 # sampling / 10-steps Flownav
                 t_sample_start = time.perf_counter()
@@ -376,20 +366,29 @@ def main(args):
                 timeline["noise_pred_ms"] = (t_noise_end - t_noise_start) * 1000.0
                 timeline["sampling_ms"] = (t_sample_end - t_sample_start) * 1000.0
 
+                # Save for logging
+                cur_naction = naction
+                cur_img = context_queue[-1]
+
+                sampled_actions_msg = Float32MultiArray()
+                sampled_action_message_data = np.concatenate((np.array([0]), naction.flatten()))
+                sampled_actions_msg.data = sampled_action_message_data.tolist()
+                sampled_actions_pub.publish(sampled_actions_msg)
+
 
                 t_proj_start = time.perf_counter()
                 current_img = context_queue[-1]
-                topomap_img = topomap[closest_node]
                 visualize_navigation(
                         current_obs_img=current_img,
-                        topomap_img=topomap_img,
+                        topomap_img=None,
                         predicted_trajectories=naction,
-                        closest_node=closest_node,
-                        goal_node=goal_node
+                        closest_node=-1,
+                        goal_node=-1,
                     )
                 t_proj_end = time.perf_counter()
                 timeline["projection_ms"] = (t_proj_end - t_proj_start) * 1000.0
-                # projection + prepare image
+                
+                # # projection + prepare image
                 # t_proj_start = time.perf_counter()
                 # projected_traj = Trajprojector.project_points(naction)
                 # last_obs = obs_images[0, -3:, :, :].detach().cpu()
@@ -411,7 +410,7 @@ def main(args):
                 # annotated_PIL = score_result.get("annotated_image")
                 # timeline["vlm_call_ms"] = (t_vlm_end - t_vlm_start) * 1000.0
 
-                # selection
+                # # selection
                 # t_sel2_start = time.perf_counter()
                 # best_idx = int(np.argmax(scores)) if scores is not None else 0
                 # t_sel2_end = time.perf_counter()
@@ -427,31 +426,19 @@ def main(args):
 
                 # print("[TIMELINE] ", json.dumps(timeline, ensure_ascii=False))
                 
-                
 
-                
+
                 # annotated_np = np.array(annotated_PIL)
                 # annotated_image_msg = msg_from_numpy(annotated_np)  # 转换为 ROS 消息格式
 
-
-                # 在主循环内，在 annotated_image_msg = msg_from_numpy(annotated_np) 之后加：
-                # goal_img_np = tensor_to_rgb_uint8(goal_images[sg_idx])
-                # if annotated_np.shape[0] != goal_img_np.shape[0]:
-                #     goal_img_np = cv2.resize(goal_img_np, (goal_img_np.shape[1], annotated_np.shape[0]))
-
-                # vis_img = np.hstack([
-                #     annotated_np,
-                #     goal_img_np,
-                # ])
                 # if args.vis_scale != 1.0:
-                #     vis_img = cv2.resize(vis_img, None, fx=args.vis_scale, fy=args.vis_scale, interpolation=cv2.INTER_LINEAR)
-                # cv2.imshow('Observation (left) vs Goal (right)', cv2.cvtColor(vis_img, cv2.COLOR_RGB2BGR))
+                #     vis_img = cv2.resize(annotated_np, None, fx=args.vis_scale, fy=args.vis_scale, interpolation=cv2.INTER_LINEAR)
+                # cv2.imshow('Observation', cv2.cvtColor(vis_img, cv2.COLOR_RGB2BGR))
                 # cv2.waitKey(1)
-  
+                
+                
                 # 发布第一个样本的指定 waypoint
                 chosen_waypoint = naction[0][args.waypoint]  #直接取第一个样本，可以加入api进行选择
-
-            print(f"[NAV] 最近节点: {closest_node} | 距离: {dists[min_idx]:.2f} | 目标: {goal_node}")
 
         # 发布 Waypoint 给 pd_controller
         
@@ -461,19 +448,10 @@ def main(args):
 
         # 发布候选轨迹
         
-        # trajs_msg.data = naction.astype(np.float32).reshape(-1).tolist()
-        # trajs_pub.publish(trajs_msg)
-        
-
         
         if annotated_image_msg is not None:
             overlay_pub.publish(annotated_image_msg) # 发布带注释的图像到 ROS 话题
-        
-        # 检查是否到达终点
-        reached_goal = closest_node == goal_node
-        goal_pub.publish(reached_goal)
-        if reached_goal:
-            print("[!] 到达终点！")
+
 
         ros_rate.sleep()
 
@@ -491,15 +469,14 @@ if __name__ == "__main__":
     parser.add_argument("--goal-node", "-g", default=-1, type=int)
     parser.add_argument("--vis-scale", default=5.0, type=float, help="可视化窗口缩放倍数（1.0=原始, 1.5=放大1.5倍等）")
     parser.add_argument("--num-samples", "-n", default=8, type=int)
-    
+    parser.add_argument(
+        "--exp_dir",
+        "-s",
+        default="explore_topomap",
+        type=str,
+        help="Path to store the exploration topomap",
+    )
+
     args = parser.parse_args()
     main(args)
 
-    # try:
-    #     main(args)
-    # except KeyboardInterrupt:
-    #     print("[!] 中断输入，退出...")
-    # except Exception as e:
-    #     print(f"[!] 错误: {e}")
-    #     import traceback
-    #     traceback.print_exc()
